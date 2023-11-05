a2bd71d42ab4d60223d59d0e77ba7dd391fa3005
Yashas Rao K <yashasrao0310@gmail.com>
2023-11-05 12:36:16 +0530

    starting after big gap

---
+++ section 32 web scraping/web_scraping.py
@@ -0,0 +1,13 @@
+# web scraping - involves programatically grabbing data from a web page
+# three steps: Download, extract data, process(do something with the data).
+
+# why scrape?
+# - no API provided by website for easy data extraction
+# - there is data on the site you want to store or analyze
+
+# is it ok?  -- yes and no
+# - some websites dont want people scraping them
+# - Best practice:consult the robots.txt file   ex:add /robots.txt to url
+# - if making many requests, time them out
+# if you are too aggressive, your IP can be blocked
+
